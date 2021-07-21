e4db0618a9d3a13cc08bfb7dbad346bf17ab00b7
정재찬 <wocks34@naver.com>
2021-07-21 09:42:36 +0900

    git commit

---
+++ src/main/java/com/jcdev/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.jcdev.book.springboot.config.auth;
+
+import com.jcdev.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity //스프링 시큐리티 설정들을 활성화시킴
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                //h2-console 화면을 사용하기 위해 disable 시킴==========================
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                //==================================================================
+                .and()
+                .authorizeRequests()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOAuth2UserService);
+    }
+
+}
